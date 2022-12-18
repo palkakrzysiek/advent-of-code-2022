0029fba21c9d1be5bd95078737f8113aa92c203a
Krzysztof Palka <palkakrzysiek@gmail.com>
2022-12-18 10:57:51 +0100

    day 15 part 2

--- day15/beacon-exclusion-zone.js
+++ day15/beacon-exclusion-zone.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const _ = require('lodash')
 
 const InputExample = fs.readFileSync('input-example.txt', 'utf8')
 const Input = fs.readFileSync('input.txt', 'utf8')
@@ -20,20 +19,30 @@ const parseLine = line => {
   }
 }
 
+const noBeaconHere = (sensorX, sensorY, beaconX, beaconY) => {
+  const maxDistance = Math.abs(beaconX - sensorX) + Math.abs(sensorY - beaconY)
+  return (whereX, whereY) => {
+    const distance = Math.abs(whereX - sensorX) + Math.abs(whereY - sensorY)
+    return distance <= maxDistance
+  }
+}
+
+const mkNoBeaconsSet = parsedLines => {
+  const beacons = new Set()
+  const mkStr = (x, y) => `${x},${y}`
+  const isBeacon = (x, y) => beacons.has(mkStr(x, y))
+
+  parsedLines.forEach(({beacon}) => beacons.add(mkStr(beacon.x, beacon.y)))
+
+  return  parsedLines.reduceRight((acc, line) => (x, y) => isBeacon(x, y) ? false :
+    noBeaconHere(line.sensor.x, line.sensor.y, line.beacon.x, line.beacon.y)(x, y) ? true : acc(x, y), () => false)
+}
+
 
 const part1 = (lines, atY) => {
-
   let minSensorX = Infinity
   let maxSensorX = -Infinity
   let maxDistanceGlobally = 0
-
-  const noBeaconHere = (sensorX, sensorY, beaconX, beaconY) => {
-    const maxDistance = Math.abs(beaconX - sensorX) + Math.abs(sensorY - beaconY)
-    return (whereX, whereY) => {
-      const distance = Math.abs(whereX - sensorX) + Math.abs(whereY - sensorY)
-      return distance <= maxDistance
-    }
-  }
 
   const parsed = lines.map(parseLine)
   parsed.forEach(({sensor, beacon}) => {
@@ -44,14 +53,7 @@ const part1 = (lines, atY) => {
 
   })
 
-  const beacons = new Set()
-  const mkStr = (x, y) => `${x},${y}`
-  const isBeacon = (x, y) => beacons.has(mkStr(x, y))
-
-  parsed.forEach(({beacon}) => beacons.add(mkStr(beacon.x, beacon.y)))
-
-  const noBeaconSet = parsed.reduceRight((acc, line) => (x, y) => isBeacon(x, y) ? false :
-    noBeaconHere(line.sensor.x, line.sensor.y, line.beacon.x, line.beacon.y)(x, y) ? true : acc(x, y), () => false)
+  const noBeaconSet = mkNoBeaconsSet(parsed)
 
   let noBeaconCounter = 0
   for (let x = minSensorX - maxDistanceGlobally; x <= maxSensorX + maxDistanceGlobally; x++) {
@@ -62,5 +64,46 @@ const part1 = (lines, atY) => {
   return noBeaconCounter
 }
 
+const point = (x, y) => {
+  return {x: x, y: y}
+}
+
+const part2 = (lines, maxCoordinate) => {
+  const parsed = lines.map(parseLine)
+
+  const noBeaconSet = mkNoBeaconsSet(parsed)
+
+  const freq = ({x, y}) => x * 4000000 + y
+
+  const canBeBeacon = ({x, y}) => {
+    if (!(x >= 0 && x <= maxCoordinate && y >= 0 && y <= maxCoordinate)) return false
+    else if (noBeaconSet(x, y)) return false
+    return true
+  }
+
+  for (const {beacon, sensor} of parsed) {
+    const maxDistance = Math.abs(beacon.x - sensor.x) + Math.abs(beacon.y - sensor.y)
+    const outlineDistance = maxDistance + 1
+
+
+    for (let i = 0; i < outlineDistance; i++) {
+      const outlinePoints = [
+        point(sensor.x - outlineDistance + i, sensor.y - i),
+        point(sensor.x - outlineDistance + i, sensor.y + i),
+        point(sensor.x + i, sensor.y - outlineDistance + i),
+        point(sensor.x + i, sensor.y + outlineDistance - i),
+      ]
+      for (const point of outlinePoints) {
+        if (canBeBeacon(point)) {
+          console.log(point)
+          return freq(point)
+        }
+      }
+    }
+  }
+}
+
 console.log(part1(splitInputLines(InputExample), 10))
+console.log(part2(splitInputLines(InputExample), 20))
 console.log(part1(splitInputLines(Input), 2000000))
+console.log(part2(splitInputLines(Input), 4000000))
